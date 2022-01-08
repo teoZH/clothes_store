dea361aa4cf85bb5b8a72ccf657d3f044ecc965c
teoZH <killdisk13@gmail.com>
2022-01-08 15:35:20 +0200

    -- class based view migration on the main crud functionality -- I used my own views which are inherited from View Class

--- base_part/urls.py
+++ base_part/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from base_part import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('details/<uuid:cloth_id>/', views.details, name='details'),
-    path('create/', views.create, name='create'),
-    path('edit/<uuid:cloth_id>/', views.edit, name='edit'),
-    path('delete/<uuid:cloth_id>/',views.delete, name='delete')
+    path('',views.IndexView.as_view(), name='index'),
+    path('details/<uuid:cloth_id>/', views.DetailsView.as_view(), name='details'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('edit/<uuid:cloth_id>/', views.EditView.as_view(), name='edit'),
+    path('delete/<uuid:cloth_id>/',views.DeleteView.as_view(), name='delete')
 ]
--- base_part/views.py
+++ base_part/views.py
@@ -4,69 +4,114 @@ from django.http import Http404
 from .forms import ClothesForm
 from .models import Clothes
 from os import remove
+from django.views import View
 
 
-def index(request):
-    clothes = Clothes.objects.all()
-    paginator = Paginator(clothes,4)
-    page_number = request.GET.get('page')
-    if not page_number:
-        page_number = 1
-    page_obj = paginator.get_page(page_number)
-    context = {'clothes': page_obj}
-    return render(request, 'index.html', context)
+class IndexView(View):
+    obj_klass = Clothes
+    objects = None
+    template_name = 'index.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.objects = self.obj_klass.objects.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        paginator = Paginator(self.objects, 4)
+        page_number = request.GET.get('page')
+        if not page_number:
+            page_number = 1
+        page_obj = paginator.get_page(page_number)
+        context = {'clothes': page_obj}
+        return render(request, self.template_name, context)
 
 
-def details(request, cloth_id):
-    cloth = get_object_or_404(Clothes, uuid=cloth_id)
-    context = {
-        'cloth': cloth,
-        'back': request.META.get('HTTP_REFERER') if request.META.get('HTTP_REFERER') else ''
-    }
-    return render(request, 'detail.html', context=context)
+class DetailsView(View):
+    obj_klass = Clothes
+    query_object = None
+    template_name = 'detail.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.query_object = get_object_or_404(self.obj_klass, uuid=kwargs['cloth_id'])
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request,cloth_id):
+        context = {
+            'cloth': self.query_object,
+            'back': request.META.get('HTTP_REFERER') if request.META.get('HTTP_REFERER') else ''
+        }
+        return render(request, self.template_name, context=context)
 
 
-def create(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            form = ClothesForm(request.POST,request.FILES)
-            if form.is_valid():
-                cloth = form.save(commit=False)
-                cloth.user = request.user
-                cloth.save()
-    context = {
-        'form': ClothesForm
-    }
-    return render(request, 'add_cloth.html', context)
+class CreateView(View):
+    form = ClothesForm
+    context = {'form': form}
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return redirect('index')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return render(request, 'add_cloth.html', self.context)
+
+    def post(self, request):
+        form = ClothesForm(request.POST, request.FILES)
+        if form.is_valid():
+            cloth = form.save(commit=False)
+            cloth.user = request.user
+            cloth.save()
+            return redirect('index')
+        return render(request, 'add_cloth.html', self.context)
 
 
-def edit(request, cloth_id):
-    try:
-        cloth = Clothes.objects.get(uuid=cloth_id)
-    except Clothes.DoesNotExist:
-        raise Http404('CLOTH DOES NOT EXIST')
-    if request.user == cloth.user:
-        if request.method == "POST":
-            form = ClothesForm(request.POST,request.FILES, instance=cloth)
-            if form.is_valid():
-                if request.FILES:
-                    remove(cloth.image.path)
-                form.save()
-                return redirect('details', cloth_id)
+class EditView(View):
+    object_klass = Clothes
+    query_object = None
+    form_klass = ClothesForm
 
-    form = ClothesForm(instance=cloth)
-    context = {
-        'form': form
-    }
-    return render(request, 'edit.html', context)
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.query_object = self.object_klass.objects.get(uuid=kwargs['cloth_id'])
+            if request.method == 'POST':
+                if not request.user == self.query_object.user:
+                    raise Http404('Not Possible!')
+        except self.object_klass.DoesNotExist:
+            redirect('index')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request,cloth_id):
+        form = self.form_klass(instance=self.query_object)
+        context = {
+            'form': form
+        }
+        return render(request, 'edit.html', context)
+
+    def post(self, request, cloth_id):
+        form = self.form_klass(request.POST, request.FILES, instance=self.object_klass)
+        if form.is_valid():
+            if request.FILES:
+                remove(self.object_klass.image.path)
+            form.save()
+            return redirect('details', cloth_id)
+        return redirect('index')
 
 
-def delete(request,cloth_id):
-    try:
-        cloth = Clothes.objects.get(uuid=cloth_id)
-        if request.user == cloth.user:
-            remove(cloth.image.path)
-            cloth.delete()
-    except Clothes.DoesNotExist:
-        raise Http404('The Resource, you requested does not exist!')
-    return redirect('index')
+class DeleteView(View):
+
+    object_klass = Clothes
+    query_object = None
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.query_object = self.object_klass.objects.get(uuid=kwargs['cloth_id'])
+        except self.object_klass.DoesNotExist:
+            return redirect('index')
+        return super().dispatch(request,*args,**kwargs)
+
+    def get(self, request,cloth_id):
+        if request.user == self.query_object.user:
+            remove(self.query_object.image.path)
+            self.query_object.delete()
+            return redirect('index')
+        raise Http404('Failed to delete')
